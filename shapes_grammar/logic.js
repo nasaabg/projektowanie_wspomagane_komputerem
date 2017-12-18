79a34bc3d4d81d7422b9ba18f4bda5f22499b818
Janek Kurzydlo <janek.kurzydlo@gmail.com>
2017-12-19 00:27:35 +0100

    Add mutation and generating productions

--- shapes_grammar/logic.js
+++ shapes_grammar/logic.js
@@ -2,11 +2,12 @@
 var currentLineSelection = {x1: 700, y1: 300, x2: 700, y2: 300}
 var currentCircleSelection = {cx: 700, cy: 300, r: 5}
 var points = []
+var productions = []
 
 // Initialization
 var svgSelection = d3.select("svg")
   .attr("width", "100%")
-  .attr("height", "1000px")
+  .attr("height", "10000px")
 
 var lineSelection = svgSelection.append("line")
   .attr("x1", currentLineSelection.x1)
@@ -26,6 +27,7 @@ var circleSelection = svgSelection.append("circle")
 var firstProd = d3.select("#firstProd");
 firstProd.on("click", function(d) {
 
+  productions.push("r")
   var y = currentLineSelection.y2
   var x2 = currentLineSelection.x2 + 40;
   var x1 = x2 - 40;
@@ -48,13 +50,14 @@ firstProd.on("click", function(d) {
   currentCircleSelection.cx = cx;
   markCurrent()
   points.push({x: currentCircleSelection.cx, y: currentCircleSelection.cy})
-  markShapes()
+  markShapes2(productions)
 });
 
 // Production 2 - down
 var secondProd = d3.select("#secondProd");
 secondProd.on("click", function(d) {
 
+  productions.push("dn")
   var x = currentLineSelection.x2;
   var y2 = currentLineSelection.y2 + 40;
   var y1 = y2 - 40;
@@ -77,13 +80,14 @@ secondProd.on("click", function(d) {
   currentCircleSelection.cy = cy;
   markCurrent()
   points.push({x: currentCircleSelection.cx, y: currentCircleSelection.cy})
-  markShapes()
+  markShapes2(productions)
 });
 
 // Production 3 - up
 var thirdProd = d3.select("#thirdProd");
 thirdProd.on("click", function(d) {
 
+  productions.push("up")
   var x = currentLineSelection.x2;
   var y2 = currentLineSelection.y2 - 40;
   var y1 = currentLineSelection.y2;
@@ -106,13 +110,14 @@ thirdProd.on("click", function(d) {
   currentCircleSelection.cy = cy;
   markCurrent()
   points.push({x: currentCircleSelection.cx, y: currentCircleSelection.cy})
-  markShapes()
+  markShapes2(productions)
 });
 
 // Production 4 - left
 var forthProd = d3.select("#forthProd");
 forthProd.on("click", function(d) {
 
+  productions.push("l")
   var y = currentLineSelection.y2
   var x2 = currentLineSelection.x2 - 40;
   var x1 = currentLineSelection.x2
@@ -135,7 +140,7 @@ forthProd.on("click", function(d) {
   currentCircleSelection.cx = cx;
   markCurrent()
   points.push({x: currentCircleSelection.cx, y: currentCircleSelection.cy})
-  markShapes()
+  markShapes2(productions)
 });
 
 function markCurrent() {
@@ -150,26 +155,6 @@ function markCurrent() {
     }
   });
 }
-
-function simulate(step) {
-  switch(step) {
-    case 1:
-      $("#firstProd").click()
-      break;
-    case 2:
-      $("#secondProd").click()
-      break;
-    case 3:
-      $("#thirdProd").click()
-      break;
-    case 4:
-      $("#forthProd").click()
-      break
-    default:
-      $("#firstProd").click()
-  }
-}
-
 
 function squ1(point) {
   var uniq_points = _.uniqWith(points, _.isEqual)
@@ -242,7 +227,7 @@ function squ4(point) {
 function markShapes() {
   var uniq_points = _.uniqWith(points, _.isEqual)
 
- var marks =  _.map(uniq_points, (p) => (
+  var marks =  _.map(uniq_points, (p) => (
     squ1(p) || squ2(p) || squ3(p) || squ4(p)
   ))
 
@@ -257,3 +242,123 @@ function markShapes() {
 
   return possitive
 }
+
+function markShapes2(productions) {
+  ratio = currentRatio(productions)
+  value = (ratio.r * ratio.dn) / (ratio.l * ratio.up)
+  return value
+}
+
+function currentRatio(productions) {
+  var counts = {
+    r: 1,
+    l: 1,
+    up: 1,
+    dn: 1,
+  };
+
+  _.each(productions, (p) => (counts[p] += 1))
+  return counts
+}
+
+
+function generateProductions(n) {
+  var values = ["r","l","up","dn"]
+  var generatedValues = []
+
+  _(n).times(function(itr) {
+    var production = values[Math.floor(Math.random() * values.length)];
+    generatedValues.push(production)
+  })
+
+  return generatedValues
+}
+
+function draw(productions) {
+  $("#ocena").text(markShapes2(productions))
+  _.each(productions, function(p) {
+    switch(p) {
+      case "r":
+        $("#firstProd").click()
+        break;
+      case "dn":
+        $("#secondProd").click()
+        break;
+      case "up":
+        $("#thirdProd").click()
+        break;
+      case "l":
+        $("#forthProd").click()
+        break
+      default:
+        $("#firstProd").click()
+    }
+  })
+}
+
+function clear() {
+  $("svg").empty();
+currentLineSelection = {x1: 700, y1: 300, x2: 700, y2: 300}
+ currentCircleSelection = {cx: 700, cy: 300, r: 5}
+ points = []
+ productions = []
+  svgSelection = d3.select("svg")
+    .attr("width", "100%")
+    .attr("height", "10000px")
+
+  circleSelection = svgSelection.append("circle")
+    .attr("cx", 700)
+    .attr("cy", 300)
+    .attr("r", 5)
+    .style("fill", "black");
+
+}
+
+function productionGroups(n) {
+  var groups = []
+
+  _(n).times(function(itr) {
+    var productions = generateProductions(_.random(0, 100))
+    groups.push(productions)
+  })
+
+  return groups
+}
+
+function findBestGroups(groups) {
+  var all_marks = []
+
+  _.each(groups, function(g, i) {
+    var obj = {
+      mark: markShapes2(g),
+      index: i
+    }
+    all_marks.push(obj)
+  })
+
+  var marks = _.sortBy(all_marks, (obj) => (obj.mark))
+  var partition =  _.partition(marks, (obj) => (obj.mark > 1))
+  var greater = _.takeRight(partition[0], 10)
+  var smaller = _.takeRight(partition[1], greater.length)
+  var bestGroups = smaller.concat(greater)
+
+  return _.map(bestGroups, (gr) => (groups[gr.index]))
+
+}
+
+
+function mutation(bestGroups) {
+  var mutation = _.flatten(bestGroups)
+  markShapes2(mutation)
+  return mutation
+}
+
+
+
+// Flow
+// var groups = productionGroups(100)
+// var bestGroups  = findBestGroups(groups)
+// var mutation = mutation(bestGroups)
+// draw(bestGroups[0])
+// clear()
+// draw(mutation)
